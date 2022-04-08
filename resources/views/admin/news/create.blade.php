b307450be7098c925460c8962eaa11cd33f2544f
鈴木 香織 <yamatosidou0810@gmail.com>
2022-04-08 03:39:38 +0000

    5th commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -8,6 +8,19 @@
         <title>MyNews</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        
+        
+        @extends('layouts.admin')
+        @section('title', 'ニュースの新規作成')
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>ニュース新規作成</h2>
+                    </div>
+                    
+                </div>
+            </div>
+        @endsection
     </body>
 </html>
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,24 @@
         <title>MyNews</title>
     </head>
     <body>
-        <h1>Myプロフィール作成画面</h1>
+        
+        @extends('layouts.profile')
+        @section('title', 'プロフィールの新規作成')
+        @section('content')
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 mx-auto">
+                        <h2>プロフィール新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+        
+        
+        
+        
+        
+        
+        
     </body>
 </html>
